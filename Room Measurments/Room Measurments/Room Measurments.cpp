451f020894651b25e8edaffe0c73dcea9476124e
Shane Wells <WS239197@weston.ac.uk>
2018-01-17 14:16:21 +0000

    Added functionnality

---
+++ Room Measurments/Room Measurments/Room Measurments.cpp
@@ -0,0 +1,22 @@
+// Room Measurments.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+#include <array>
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+class
+{
+
+
+};
+
+int main()
+{
+    return 0;
+}
+
