04865f8fc5b6eaf8e3148a64f70deb483ee993cf
Shane130798 <32132593+Shane130798@users.noreply.github.com>
2018-01-17 15:10:53 +0000

    V1.1 (#2)
    * Simple structure changes
    
    * added functionality
    
    * Added functionality for one wall calculation.
    
    * b

--- Room Measurments/Room Measurments/Room Measurments.cpp
+++ Room Measurments/Room Measurments/Room Measurments.cpp
@@ -9,14 +9,58 @@
 
 using namespace std;
 
-class
+class Wall
 {
-
+public:
+	void GetWallDetails(double dWidthpass);
+	double ShowResults(double dHeightpass, double dWidthpass);
+protected:
+	 
+private:
+	double dHeight[2];
+	double dWidth =0; 
 
 };
 
+void Wall::GetWallDetails(double dWidthpass) 
+{
+	dWidth = dWidthpass;
+};
+
+
+
+double Wall::ShowResults(double dHeightpass, double dWidthpass)
+{	
+	double  dArea = dHeight[2] * dWidth;
+	double dPrice = dArea * 1;
+	double dPriceDeluxe = dArea * 12;
+	cout << endl << endl << "The Price of Normal Wallpaper for this wall is: " << char(156) << dPrice;
+	cout << endl << endl << "The Price of Deluxe Wallpaper for this wall is: " << char(156) << dPriceDeluxe;
+
+	return dPrice;
+}
+
 int main()
 {
+	Wall oWall;
+	double dHeight = 0;
+	double dWidth = 0;
+	cout << "Welcome to the program!" << endl;
+	cout << endl << "Please enter the Wall's Width in Meters: ";
+	cin >> dWidth;
+	cout << endl << "Your Wall's Width is: " << dWidth << " M";
+
+	oWall.GetWallDetails(dWidth);
+	oWall.ShowResults(dHeight, dWidth);
+
+
+
+
+
+
+
+
+	_getch();
     return 0;
 }
 
